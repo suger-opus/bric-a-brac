e31f1cec051f9046c9b472eabfe778c78113ed69
Adrien Fort <suger-opus@proton.me>
2026-01-22 15:11:21 +0100

    app/feat: improve error mngt when creating node type

--- web-ui/components/dialog-contents/new-node-dialog-content.tsx
+++ web-ui/components/dialog-contents/new-node-dialog-content.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ColorPickerField from "@/components/forms/color-picker-field";
-import PropertyField from "@/components/forms/property-field";
+import PropertyForm from "@/components/forms/property-form";
 import { Button } from "@/components/ui/button";
 import {
   DialogContent,
@@ -10,25 +10,19 @@ import {
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog";
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldSet
-} from "@/components/ui/field";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   requestColor,
   requestFormattedLabel,
-  requestLabel
+  requestLabel,
+  requestProperty
 } from "@/lib/api/schemas/request-schemas";
 import { filterLabel, formatLabel } from "@/lib/utils";
 import { RequestProperty } from "@/types";
 import { defaultNewProperty } from "@/types/defaults";
-import { PlusIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, PlusIcon } from "lucide-react";
 import { useEffect, useEffectEvent, useState } from "react";
 import * as v from "valibot";
 
@@ -49,28 +43,48 @@ const NewNodeDialogContent = ({ isOpen, onClose }: NewNodeDialogContentProps) =>
   );
   const [color, setColor] = useState("#3b82f6");
   const [colorValidationError, setColorValidationError] = useState<string | null>(null);
-  const [draftProperties, setDraftProperties] = useState<RequestProperty[]>([]);
-  const [properties, setProperties] = useState<RequestProperty[]>([]);
+  const [properties, setProperties] = useState<
+    { id: string; isSaved: boolean; property: RequestProperty; }[]
+  >([]);
+  const [propertyErrors, setPropertyErrors] = useState<Record<string, string | null>>({});
+  const unresolvedError = labelValidationError !== null
+    || formattedLabelValidationError !== null
+    || colorValidationError !== null
+    || Object.values(propertyErrors).some((error) => error !== null);
 
   const handleLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLabel(filterLabel(e.target.value));
   };
 
-  const handleAddDraftProperty = () => {
-    setDraftProperties([...draftProperties, defaultNewProperty]);
+  const handleAddProperty = () => {
+    setProperties([...properties, {
+      id: crypto.randomUUID(),
+      isSaved: false,
+      property: defaultNewProperty
+    }]);
   };
 
-  const handleRemoveDraftProperty = (index: number) => {
-    setDraftProperties(draftProperties.filter((_, i) => i !== index));
+  const handleDeleteProperty = (id: string) => {
+    setProperties(properties.filter((property) => property.id !== id));
   };
 
-  const handleAddProperty = (index: number, property: RequestProperty) => {
-    setProperties([...properties, property]);
-    handleRemoveDraftProperty(index);
+  const handleSaveProperty = (id: string, newProperty: RequestProperty) => {
+    setProperties(properties.map((property) => {
+      if (property.id === id) {
+        return { id, isSaved: true, property: newProperty };
+      }
+      return property;
+    }));
+    setPropertyErrors((prev) => ({ ...prev, [id]: null }));
   };
 
-  const handleRemoveProperty = (index: number) => {
-    setProperties(properties.filter((_, i) => i !== index));
+  const handleUnsaveProperty = (id: string) => {
+    setProperties(properties.map((property) => {
+      if (property.id === id) {
+        return { ...property, isSaved: false };
+      }
+      return property;
+    }));
   };
 
   const handlePreviousPage = () => {
@@ -83,13 +97,15 @@ const NewNodeDialogContent = ({ isOpen, onClose }: NewNodeDialogContentProps) =>
       const validFormattedLabel = v.safeParse(requestFormattedLabel, formattedLabel);
       if (!validLabel.success) {
         setLabelValidationError(validLabel.issues[0].message);
+      } else {
+        setLabelValidationError(null);
       }
       if (!validFormattedLabel.success) {
         setFormattedLabelValidationError(validFormattedLabel.issues[0].message);
+      } else {
+        setFormattedLabelValidationError(null);
       }
       if (validLabel.success && validFormattedLabel.success) {
-        setLabelValidationError(null);
-        setFormattedLabelValidationError(null);
         setCurrentStep(1);
       }
     } else if (currentStep === 1) {
@@ -102,6 +118,24 @@ const NewNodeDialogContent = ({ isOpen, onClose }: NewNodeDialogContentProps) =>
         setCurrentStep(2);
       }
     } else if (currentStep === 2) {
+      properties.forEach((property) => {
+        if (!property.isSaved) {
+          setPropertyErrors((prev) => ({
+            ...prev,
+            [property.id]: "Save this property to continue."
+          }));
+        } else {
+          const validProperty = v.safeParse(requestProperty, property.property);
+          if (!validProperty.success) {
+            setPropertyErrors((prev) => ({
+              ...prev,
+              [property.id]: validProperty.issues[0].message
+            }));
+          } else {
+            setPropertyErrors((prev) => ({ ...prev, [property.id]: null }));
+          }
+        }
+      });
     }
   };
 
@@ -114,6 +148,7 @@ const NewNodeDialogContent = ({ isOpen, onClose }: NewNodeDialogContentProps) =>
     setColor("#3b82f6");
     setColorValidationError(null);
     setProperties([]);
+    setPropertyErrors({});
   });
 
   useEffect(() => {
@@ -132,101 +167,113 @@ const NewNodeDialogContent = ({ isOpen, onClose }: NewNodeDialogContentProps) =>
         <Tabs value={steps[currentStep]}>
           <TabsList className="w-full mb-4">
             <TabsTrigger value={steps[0]} disabled={currentStep !== 0}>
-              {steps[0]}
+              {steps[0]} {currentStep > 0
+                ? <CheckIcon className="ml-auto" />
+                : <ChevronRightIcon className="ml-auto" />}
             </TabsTrigger>
             <TabsTrigger value={steps[1]} disabled={currentStep !== 1}>
-              {steps[1]}
+              {steps[1]} {currentStep > 1
+                ? <CheckIcon className="ml-auto" />
+                : <ChevronRightIcon className="ml-auto" />}
             </TabsTrigger>
             <TabsTrigger value={steps[2]} disabled={currentStep !== 2}>
-              {steps[2]}
+              {steps[2]} <ChevronRightIcon className="ml-auto" />
             </TabsTrigger>
           </TabsList>
           <TabsContent value={steps[0]}>
-            <FieldSet>
-              <FieldGroup>
-                <Field>
-                  <FieldLabel htmlFor="new-node-label">Label</FieldLabel>
-                  <FieldDescription className="text-xs">
-                    The label is the name of the node type.{" "}
-                    <b>
-                      Only letters and spaces are allowed.
-                    </b>
-                  </FieldDescription>
-                  <Input
-                    id="new-node-label"
-                    value={label}
-                    onChange={handleLabelChange}
-                    placeholder="Character"
-                  />
-                  <FieldError>{labelValidationError}</FieldError>
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="new-node-formatted-label">Formatted Label</FieldLabel>
-                  <FieldDescription className="text-xs">
-                    The formatted label is generated automatically.{" "}
-                    <b>
-                      It should be unique among nodes and edges types of this graph.
-                    </b>
-                  </FieldDescription>
-                  <Input id="new-node-formatted-label" value={formattedLabel} readOnly />
-                  <FieldError>{formattedLabelValidationError}</FieldError>
-                </Field>
-              </FieldGroup>
-            </FieldSet>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="new-node-label">Label</FieldLabel>
+                <FieldDescription className="text-xs">
+                  The label is the name of the node type.{" "}
+                  <b>
+                    Only letters and spaces are allowed.
+                  </b>
+                </FieldDescription>
+                <Input
+                  id="new-node-label"
+                  value={label}
+                  onChange={handleLabelChange}
+                  placeholder="Character"
+                />
+                <FieldError>{labelValidationError}</FieldError>
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="new-node-formatted-label">Formatted Label</FieldLabel>
+                <FieldDescription className="text-xs">
+                  The formatted label is generated automatically.{" "}
+                  <b>
+                    It should be unique among nodes and edges types of this graph.
+                  </b>
+                </FieldDescription>
+                <Input id="new-node-formatted-label" value={formattedLabel} readOnly />
+                <FieldError>{formattedLabelValidationError}</FieldError>
+              </Field>
+            </FieldGroup>
           </TabsContent>
           <TabsContent value={steps[1]}>
-            <FieldSet>
-              <FieldGroup>
-                <ColorPickerField
-                  color={color}
-                  setColor={setColor}
-                  validationError={colorValidationError}
-                />
-              </FieldGroup>
-            </FieldSet>
+            <FieldGroup>
+              <ColorPickerField
+                color={color}
+                setColor={setColor}
+                validationError={colorValidationError}
+              />
+            </FieldGroup>
           </TabsContent>
           <TabsContent value={steps[2]}>
-            <FieldSet>
-              <FieldGroup>
-                {draftProperties.map((property, index) => (
-                  <PropertyField
-                    key={index}
-                    index={index}
-                    property={property}
-                    addProperty={(p) => handleAddProperty(index, p)}
-                    removeProperty={() => handleRemoveDraftProperty(index)}
-                  />
+            <FieldGroup>
+              <div className="space-y-4">
+                {properties.map((property, index) => (
+                  <div key={index} className="space-y-1">
+                    <PropertyForm
+                      isSaved={property.isSaved}
+                      property={property.property}
+                      saveProperty={(p) => handleSaveProperty(property.id, p)}
+                      unSaveProperty={() => handleUnsaveProperty(property.id)}
+                      deleteProperty={() => handleDeleteProperty(property.id)}
+                    />
+                    {propertyErrors[property.id] && (
+                      <FieldError key={index}>{propertyErrors[property.id]}</FieldError>
+                    )}
+                  </div>
                 ))}
-                <Field>
-                  <Button size="sm" variant="outline" onClick={handleAddDraftProperty}>
-                    <PlusIcon />Add Custom Property
-                  </Button>
-                </Field>
-              </FieldGroup>
-            </FieldSet>
+              </div>
+              <Field>
+                <Button size="sm" variant="outline" onClick={handleAddProperty}>
+                  <PlusIcon />Add Custom Property
+                </Button>
+              </Field>
+            </FieldGroup>
           </TabsContent>
         </Tabs>
       </div>
       <DialogFooter className="h-fit">
-        {currentStep === 0
-          ? (
-            <Button className="ml-auto" variant="outline" onClick={onClose}>
-              Cancel
-            </Button>
-          )
-          : (
+        <div className="flex flex-col w-full items-end space-y-2">
+          <div className="flex space-x-2">
+            {currentStep === 0
+              ? (
+                <Button variant="outline" onClick={onClose}>
+                  Cancel
+                </Button>
+              )
+              : (
+                <Button
+                  variant="outline"
+                  onClick={handlePreviousPage}
+                >
+                  Back
+                </Button>
+              )}
             <Button
-              variant="outline"
-              onClick={handlePreviousPage}
+              onClick={handleNextPage}
             >
-              Back
+              {currentStep === steps.length - 1 ? "Submit" : "Next"}
             </Button>
+          </div>
+          {unresolvedError && (
+            <FieldError>Unresolved errors. Please fix them before continuing.</FieldError>
           )}
-        <Button
-          onClick={handleNextPage}
-        >
-          {currentStep === steps.length - 1 ? "Submit" : "Next"}
-        </Button>
+        </div>
       </DialogFooter>
     </DialogContent>
   );
---
+++ web-ui/components/forms/property-form.tsx
@@ -0,0 +1,314 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+import {
+  requestFormattedLabel,
+  requestLabel,
+  requestPropertyMetadata
+} from "@/lib/api/schemas/request-schemas";
+import { filterLabel, formatLabel, pluralize } from "@/lib/utils";
+import { PropertyType, RequestProperty } from "@/types";
+import { XIcon } from "lucide-react";
+import { useState } from "react";
+import * as v from "valibot";
+
+type PropertyFieldGroupProps = {
+  baseProperty: RequestProperty;
+  saveProperty: (property: RequestProperty) => void;
+  deleteProperty: () => void;
+};
+
+const PropertyFieldGroup = (
+  { baseProperty, saveProperty, deleteProperty }: PropertyFieldGroupProps
+) => {
+  const [label, setLabel] = useState(baseProperty.label);
+  const [labelValidationError, setLabelValidationError] = useState<string | null>(null);
+  const formattedLabel = formatLabel(label);
+  const [formattedLabelValidationError, setFormattedLabelValidationError] = useState<string | null>(
+    null
+  );
+  const [propertyType, setPropertyType] = useState(baseProperty.metadata.property_type);
+  const [isPropertyRequired, setIsPropertyRequired] = useState(
+    baseProperty.metadata.details.required
+  );
+  const [currentOption, setCurrentOption] = useState("");
+  const [options, setOptions] = useState(baseProperty.metadata.details.options);
+  const [metadataValidationError, setMetadataValidationError] = useState<string | null>(null);
+
+  const handleLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLabel(filterLabel(e.target.value));
+  };
+
+  const handlePropertyTypeChange = (value: PropertyType) => {
+    setPropertyType(value);
+    if (value !== PropertyType.SELECT) {
+      setOptions(null);
+    } else {
+      setOptions([]);
+    }
+  };
+
+  const handleOptionInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentOption(e.target.value);
+  };
+
+  const handleOptionInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && currentOption.trim() !== "") {
+      e.preventDefault();
+      if (options) {
+        setOptions([...options, currentOption.trim()]);
+      } else {
+        setOptions([currentOption.trim()]);
+      }
+      setCurrentOption("");
+    }
+  };
+
+  const handlePropertyOptionRemoved = (optionToRemove: string) => {
+    if (options) {
+      const filteredOptions = options.filter((option) => option !== optionToRemove);
+      setOptions(filteredOptions);
+    }
+  };
+
+  const handleAddProperty = () => {
+    const validLabel = v.safeParse(requestLabel, label);
+    const validFormattedLabel = v.safeParse(requestFormattedLabel, formattedLabel);
+    const validMetadata = v.safeParse(requestPropertyMetadata, {
+      property_type: propertyType,
+      details: {
+        options: options,
+        required: isPropertyRequired
+      }
+    });
+    if (!validLabel.success) {
+      setLabelValidationError(validLabel.issues[0].message);
+    } else {
+      setLabelValidationError(null);
+    }
+    if (!validFormattedLabel.success) {
+      setFormattedLabelValidationError(validFormattedLabel.issues[0].message);
+    } else {
+      setFormattedLabelValidationError(null);
+    }
+    if (!validMetadata.success) {
+      setMetadataValidationError(validMetadata.issues[0].message);
+    } else {
+      setMetadataValidationError(null);
+    }
+    if (validLabel.success && validFormattedLabel.success && validMetadata.success) {
+      saveProperty({
+        label,
+        formatted_label: formattedLabel,
+        metadata: {
+          property_type: propertyType,
+          details: {
+            options: options,
+            required: isPropertyRequired
+          }
+        }
+      });
+    }
+  };
+
+  const handleRemoveProperty = () => {
+    deleteProperty();
+  };
+
+  return (
+    <ItemContent>
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="new-property-label">Label</FieldLabel>
+          <FieldDescription className="text-xs">
+            The label is the name of the property.{" "}
+            <b>
+              Only letters and spaces are allowed.
+            </b>
+          </FieldDescription>
+          <Input
+            id="new-property-label"
+            value={label}
+            onChange={handleLabelChange}
+            placeholder="Character"
+          />
+          <FieldError>{labelValidationError}</FieldError>
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="new-property-formatted-label">Formatted Label</FieldLabel>
+          <FieldDescription className="text-xs">
+            The formatted label is generated automatically.{" "}
+            <b>
+              It should be unique among properties of this node/edge type.
+            </b>
+          </FieldDescription>
+          <Input id="new-property-formatted-label" value={formattedLabel} readOnly />
+          <FieldError>{formattedLabelValidationError}</FieldError>
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="new-property-type">Type</FieldLabel>
+          <FieldDescription className="text-xs">
+            The type defines the kind of data this property can hold.
+          </FieldDescription>
+          <Select value={propertyType} onValueChange={handlePropertyTypeChange}>
+            <SelectTrigger className="w-full" id="new-property-type">
+              <SelectValue placeholder="Select a type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {Object.values(PropertyType).map((type) => (
+                  <SelectItem key={type} value={type}>
+                    {type}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </Field>
+        {propertyType === PropertyType.SELECT && (
+          <Field>
+            <FieldLabel htmlFor="new-property-options">Options</FieldLabel>
+            <FieldDescription className="text-xs">
+              Define the selectable options for this property. Type and press Enter to add an
+              option.
+            </FieldDescription>
+            <InputGroup>
+              <InputGroupInput
+                placeholder="Yellow"
+                value={currentOption}
+                onChange={handleOptionInputChange}
+                onKeyDown={handleOptionInputKeyDown}
+              />
+              <InputGroupAddon align="block-end" className="flex flex-wrap">
+                {options?.map((option, idx) => (
+                  <Badge key={idx} variant="secondary">
+                    {option}
+                    <Button
+                      variant="ghost"
+                      size="icon-sm"
+                      className="cursor-pointer p-0 h-3 w-3"
+                      onClick={() => handlePropertyOptionRemoved(option)}
+                    >
+                      <XIcon />
+                    </Button>
+                  </Badge>
+                ))}
+              </InputGroupAddon>
+              <InputGroupAddon align="block-end">
+                <FieldDescription className="text-xs">
+                  {options?.length} {pluralize(options?.length || 0, "option", "options")}
+                </FieldDescription>
+              </InputGroupAddon>
+            </InputGroup>
+            <FieldError>{metadataValidationError}</FieldError>
+          </Field>
+        )}
+        <Field orientation="horizontal">
+          <Checkbox
+            id="new-property-required"
+            checked={isPropertyRequired}
+            onCheckedChange={(e) => setIsPropertyRequired(e as boolean)}
+          />
+          <FieldLabel
+            htmlFor="new-property-required"
+            className="font-normal"
+          >
+            I want this property to be required
+          </FieldLabel>
+        </Field>
+        <FieldSeparator />
+        <Field orientation="horizontal">
+          <Button size="sm" variant="secondary" onClick={handleAddProperty}>
+            Save Property
+          </Button>
+          <Button size="sm" variant="ghost" onClick={handleRemoveProperty}>
+            Remove
+          </Button>
+        </Field>
+      </FieldGroup>
+    </ItemContent>
+  );
+};
+
+type PropertyItemProps = {
+  property: RequestProperty;
+  updateProperty: () => void;
+  removeProperty: () => void;
+};
+
+const PropertyItem = ({ property, updateProperty, removeProperty }: PropertyItemProps) => {
+  return (
+    <>
+      <ItemContent>
+        <ItemTitle>{property.label}</ItemTitle>
+        <ItemDescription className="space-x-1">
+          <Badge variant="secondary">{property.metadata.property_type}</Badge>
+          <Badge variant="secondary">
+            {property.metadata.details.required ? "required" : "optional"}
+          </Badge>
+        </ItemDescription>
+      </ItemContent>
+      <ItemActions>
+        <Button size="sm" variant="outline" onClick={updateProperty}>
+          Edit
+        </Button>
+        <Button size="sm" variant="outline" onClick={removeProperty}>
+          Delete
+        </Button>
+      </ItemActions>
+    </>
+  );
+};
+
+type PropertyFormProps = {
+  property: RequestProperty;
+  isSaved: boolean;
+  saveProperty: (property: RequestProperty) => void;
+  unSaveProperty: () => void;
+  deleteProperty: () => void;
+};
+
+const PropertyForm = (
+  { property, isSaved, saveProperty, unSaveProperty, deleteProperty }: PropertyFormProps
+) => {
+  return (
+    <Item variant="outline">
+      {isSaved
+        ? (
+          <PropertyItem
+            property={property}
+            updateProperty={unSaveProperty}
+            removeProperty={deleteProperty}
+          />
+        )
+        : (
+          <PropertyFieldGroup
+            baseProperty={property}
+            saveProperty={saveProperty}
+            deleteProperty={deleteProperty}
+          />
+        )}
+    </Item>
+  );
+};
+
+export default PropertyForm;
